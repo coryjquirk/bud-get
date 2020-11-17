398a49cf6c0dbb80839167ab166d06b1b79eab28
Cory Quirk <coryjquirk@gmail.com>
2020-11-16 22:55:49 -0600

    fixed conxn string

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGOATLAS_URI || "mongodb:localhost/budget";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
