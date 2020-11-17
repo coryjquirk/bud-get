a916433593af3b4d34f6925f7f699a4ee34ce38d
Cory Quirk <coryjquirk@gmail.com>
2020-11-16 22:52:12 -0600

    indexedDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,10 @@
 console.log("Hallo from your service worker!")
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("service-worker.js")
+        .then(reg => {
+          console.log("We found your service worker file!", reg);
+        });
+    });
+  }
